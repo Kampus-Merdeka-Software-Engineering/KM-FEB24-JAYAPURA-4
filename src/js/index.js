5b758ea7cc0d569fc9de4514ea0e828f3f0b5be8
Qalbi Karunia <101724176+Qalbikarunia@users.noreply.github.com>
2024-05-20 07:55:34 +0700

    dark mode

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
+var moonIcn = document.getElementById('icn')
+moonIcn.addEventListener('click', function () {
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains("dark-theme")){
+        moonIcn.src = 'src/img/sun.png'
+    } else {
+        moonIcn.src = 'src/img/moon.png'
+    }
+} )
+
 const menuBar = document.querySelector('#side-bar .sidebar-header .bx.bx-menu');
 const sidebar = document.getElementById('side-bar');
 
@@ -19,6 +29,17 @@ searchButton.addEventListener('click', function (e){
             searchButtonIcon.classList.replace('bx-x', 'bx-search')
         }
     }
+})
+
+const menuList = document.querySelectorAll('#side-bar .nav-sidebar .list-item')
+
+menuList.forEach(function (item) {
+    item.addEventListener('click', function () {
+        if (window.innerWidth < 576){
+            sidebar.classList.remove('active'); 
+        }
+    })
+    
 })
 
 window.addEventListener('resize', function(){
