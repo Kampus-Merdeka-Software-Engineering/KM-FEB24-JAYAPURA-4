5326ffa231fea8622d2c2297b933fe4c0c179238
Qalbi Karunia <101724176+Qalbikarunia@users.noreply.github.com>
2024-06-10 19:52:23 +0700

    set theme

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,9 +1,26 @@
 var moonIcn = document.getElementById('icn')
+
+// Function to set the theme based on localStorage value
+function setTheme() {
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-theme');
+        moonIcn.src = 'src/img/sun.png';
+    } else {
+        document.body.classList.remove('dark-theme');
+        moonIcn.src = 'src/img/moon.png';
+    }
+}
+
+// Set the initial theme on page load
+setTheme();
+
  moonIcn.onclick = function () {
     document.body.classList.toggle('dark-theme');
     if (document.body.classList.contains("dark-theme")) {
-        moonIcn.src = 'src/img/sun.png'
+        moonIcn.src = 'src/img/sun.png';
+        localStorage.setItem('theme', 'dark');
     } else {
-        moonIcn.src = 'src/img/moon.png'
+        moonIcn.src = 'src/img/moon.png';
+        localStorage.setItem('theme', 'light');
     }
 }
