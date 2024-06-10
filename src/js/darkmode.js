7192555fb73c4ee0b2c1bb97fba0e78a3b67bea0
Qalbi Karunia <101724176+Qalbikarunia@users.noreply.github.com>
2024-06-10 18:53:08 +0700

    dark theme.2

--- src/js/darkmode.js
+++ src/js/darkmode.js
@@ -1,9 +1,9 @@
 var moonIcn = document.getElementById('icn')
-moonIcn.addEventListener('click', function () {
+ moonIcn.onclick = function () {
     document.body.classList.toggle('dark-theme');
     if (document.body.classList.contains("dark-theme")) {
         moonIcn.src = 'src/img/sun.png'
     } else {
         moonIcn.src = 'src/img/moon.png'
     }
-})
+}
